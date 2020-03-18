6b8ad843cd579796e7a0d2082a40a4dc3238eb9b
scsd3211 <binghunxuluo@qq.com>
2020-03-18 23:57:57 +0800

    modify startIndex

--- startIndex.py
+++ startIndex.py
@@ -1,6 +1,11 @@
 from flask import Flask
+print("asda")
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'chenzhuo Hello, World!'
+
+if __name__ == '__main__':
+    print("nihio")
+    app.run()
